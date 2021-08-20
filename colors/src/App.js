fffecfe1c716b5cc984dc1b375d9b9999f219cdf
bdushimi <lespharaons@hotmail.com>
2021-08-20 18:29:20 +0200

    Adds ColorHepler to generate colors

--- colors/src/App.js
+++ colors/src/App.js
@@ -1,7 +1,9 @@
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette} from "./colorsHelper";
 
 function App() {
+  console.log(generatePalette(seedColors[2]))
   return (
     <div>
       <Palette {...seedColors[2]}/>
