56bf427b260c3a9d87f625db48b8a935ec6db16f
bdushimi <lespharaons@hotmail.com>
2021-08-27 18:12:19 +0200

    Add functional routes

--- colors/src/App.js
+++ colors/src/App.js
@@ -6,15 +6,21 @@ import { generatePalette} from "./colorsHelper";
 function App() {
    
   const findPalette = (id) => {
-    seedColors.find(palette => {
+    return seedColors.find((palette) => {
       return palette.id === id;
     })
   }
-
   return (
     <Switch>
-      <Route exact path="/" render={ () => <h1>PALETTE LIST GOES HERE</h1>}/>
-      <Route exact path="/palette/:id" render={() => <h1>INDIVIDUAL GOES HERE</h1>}/>
+      <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+      
+      <Route
+        exact path="/palette/:id"
+        render={(routeProps) =>
+          <Palette palette={generatePalette(findPalette(routeProps.match.params.id))}
+          />
+        }
+      />
     </Switch>
     // <div>
     //   <Palette palette={generatePalette(seedColors[4])}/>
