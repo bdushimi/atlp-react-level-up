c9867308fc58013f0313557722ef3b845076222b
bdushimi <lespharaons@hotmail.com>
2021-09-01 10:59:40 +0200

    Finish refactor of Colorbox css

--- colors/src/colorBox.js
+++ colors/src/colorBox.js
@@ -11,7 +11,7 @@ const styles = {
 
     ColorBox: {
         width: "20%",
-        height: props => props.showFullPalette ? "25%": "50%",
+        height: props => props.showFullPalette ? "25%" : "50%",
         margin: "0 auto",
         display: "inline-block",
         position: "relative",
@@ -63,10 +63,72 @@ const styles = {
         textTransform: "uppercase",
         border: "none",
         opacity: 0
+    },
+
+    boxContent: {
+        position: "absolute",
+        padding: "10px",
+        width: "100%",
+        left: "0",
+        bottom: "0",
+        color: "black",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        fontSize: "12px",
+    },
+
+    copyOverlay: {
+        opacity: "0",
+        zIndex: "0",
+        width: "100%",
+        height: "100%",
+        transition: "transform 0.5s ease-in -out",
+    },
+    showOverlay: {
+        opacity: "1",
+        transform: "scale(50)",
+        zIndex: "10",
+        position: "absolute",
+    },
+    copyMessage: {
+        position: "fixed",
+        top: "0",
+        left: "0",
+        right: "0",
+        bottom: "0",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "4rem",
+        transform: "scale(0.1)",
+        opacity: "0",
+        color: "#fff",
+        textTransform: "uppercase",
+        "& h1": {
+            fontWeight: "400",
+            textShadow: "1px 2px black",
+            backgroundColor: "rgba(255, 255, 255, 0.2)",
+            width: "100%",
+            textAlign: "center",
+            padding: "1rem",
+            marginBottom: "0",
+            textTransform: "uppercase",
+        },
+
+        "& p": {
+            fontSize: "2rem",
+            fontWeight: "100"
+        }
+    },
+
+    showCopyMessage: {
+        opacity: "1",
+        zIndex: "20",
+        transform: "scale(1)",
+        transition: "all 0.2s",
+        transitionDelay: "0.3s",
     }
-
-
-
 }
 class colorBox extends Component {
     constructor(props) {
@@ -89,22 +151,22 @@ class colorBox extends Component {
 
 
     render() {
-        const { name, background, paletteId, id, showLink, classes, showFullPalette} = this.props;
+        const { name, background, paletteId, id, showLink, classes, showFullPalette } = this.props;
         const { copied } = this.state;
 
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-                <div style={{ background }} className={ classes.ColorBox}>
+                <div style={{ background }} className={classes.ColorBox}>
                     <div style={{ background }}
-                        className={`copy-overlay ${copied && "show"}`}>
+                        className={`${classes.copyOverlay} ${copied && classes.showOverlay}`}>
 
                     </div>
-                    <div className={`copy-msg ${copied && "show"}`}>
+                    <div className={`${classes.copyMessage} ${copied && classes.showCopyMessage}`}>
                         <h1>copied!</h1>
                         <p className={classes.copyText}>{background}</p>
                     </div>
-                    <div className="copy-container">
-                        <div className="box-content">
+                    <div>
+                        <div className={classes.boxContent}>
                             <span className={classes.colorName}>{name}</span>
                         </div>
 
