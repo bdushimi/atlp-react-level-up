7c6d98231282387960229165ad1c9c7c9a7b00c8
bdushimi <lespharaons@hotmail.com>
2022-11-17 21:37:56 +0200

    Adds loading indicator when waiting for api call to return data

--- react-query-essentials/src/App.js
+++ react-query-essentials/src/App.js
@@ -3,15 +3,22 @@ import {useQuery } from 'react-query';
 import axios from 'axios';
 
 function App() {
-  const queryInfo = useQuery('pokemon',
-    () => axios
+  const queryInfo = useQuery('pokemon', async () => {
+    // Make the api call wait for 2sec
+    // await new Promise(resolve => setTimeout(resolve, 2000))
+    return axios
       .get('https://pokeapi.co/api/v2/pokemon')
-      .then(res => res.data.results))
-  return (
-    <div>{queryInfo.data?.map(result => {
-      return <div key={result.name}>{ result.name}</div>
-    })}</div>
-  );
+      .then(res => res.data.results)
+  })
+  
+  
+  return queryInfo.isLoading ?
+    ('Loading.....')
+    : (
+      <div>{queryInfo.data.map(result => {
+        return <div key={result.name}>{result.name}</div>
+      })}</div>
+    )
 }
 
 export default App;
