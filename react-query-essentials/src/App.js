56f560f1ca366a13293aaaaa95df26be57363765
bdushimi <lespharaons@hotmail.com>
2022-11-17 21:29:31 +0200

    Adds first react query

--- react-query-essentials/src/App.js
+++ react-query-essentials/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useQuery } from 'react-query';
+import axios from 'axios';
 
 function App() {
+  const queryInfo = useQuery('pokemon',
+    () => axios
+      .get('https://pokeapi.co/api/v2/pokemon')
+      .then(res => res.data.results))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>{queryInfo.data?.map(result => {
+      return <div key={result.name}>{ result.name}</div>
+    })}</div>
   );
 }
 
